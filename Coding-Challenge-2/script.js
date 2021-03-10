b5ac874e963ff269eb43adc012d6c4f4926d4126
Oguzhanolgun <oguzhanolgun35.5@gmail.com>
2021-03-10 15:58:17 +0300

    Refactored calcTip function

--- Coding-Challenge-2/script.js
+++ Coding-Challenge-2/script.js
@@ -10,12 +10,7 @@ TEST DATA: 125, 555 and 44
 HINT: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
 */
 
-const calcTip = (bill) => {
-      let tip;
-      if(bill >= 50 && bill <= 300 ) return tip = bill * .15;
-      return  bill * .20;  
-};
-
+const calcTip = (bill) => (bill >= 50 && bill <= 300 ) ? tip = bill * .15 : bill * .20 ;  
 const bills = [125, 555, 44];
 const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2]) ];
 const total = [bills[0] +tips[0], bills[1] + tips[1], bills[2] + tips[2] ];
