0913d394796d416b96d0b10a96f31ac266de3157
Oguzhanolgun <oguzhanolgun35.5@gmail.com>
2021-04-05 03:24:56 +0300

    Added solution for coding challenge 8

---
+++ Coding-Challenge-8/script.js
@@ -0,0 +1,52 @@
+// Coding Challenge #8
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+*/
+
+// 1. Method
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const textArr = text.split('\n');
+  for (const [i, el] of textArr.entries()) {
+    const newStr = el.toLowerCase().trim().split('_');
+    const [first, second] = newStr;
+    const upperSecond = second[0].toUpperCase() + second.slice(1);
+    const lastStr = first.concat(upperSecond);
+    const finalStr = lastStr.padEnd(20) + '✔'.repeat(i + 1);
+    console.log(finalStr);
+  }
+});
+
+// 2. Method
+/*
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split('_');
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+  }
+});
+*/
